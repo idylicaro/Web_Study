30df22d221e0d320067e4d1b65abb4fd88aade34
Idyl Icaro dos Santos <idylicaro.se@hotmaill.com>
2020-05-03 21:08:55 -0300

    [IMP] core  game SNAKE GAME

--- snakeGame/index.js
+++ snakeGame/index.js
@@ -1,73 +1,110 @@
+var sizeGlobal = 20; //size to px
+var colsAndRows;
+
 function setup() {
-  createCanvas(720, 400);
-  
-  userSnake = new snake();
+  frameRate(10);
+  createCanvas(600, 600);
+  colsAndRows = pickLocation();
+  userSnake   = new snake();
+  var food    = pickLocation();
 }
 
 function draw() {
   background(51);
-  //keyPressed();
-  userSnake.updatePosition();
+  userSnake.death();
+  userSnake.update();
   userSnake.show();
+  fill(255,0,100);
+  square(food.x,food.y,sizeGlobal);
+  
+  if(userSnake.eat(food)){
+      userSnake.bodySize++;
+      pickLocation();
+  }
+  
 }
 
 function keyPressed() {
   switch (keyCode) {
     case UP_ARROW:
-      userSnake.setDirection('W');
+      userSnake.setSpeed(0,-1);
       break;
     case DOWN_ARROW:
-      userSnake.setDirection('S');
+      userSnake.setSpeed(0,1);
       break;
     case LEFT_ARROW:
-      userSnake.setDirection('A');
+      userSnake.setSpeed(-1,0);
       break;
     case RIGHT_ARROW:
-      userSnake.setDirection('D');
+      userSnake.setSpeed(1,0);
       break;
   }
 }
-function snake() {
-  this.keyDirection;
-  this.position = [360, 200]; //x,y
-  this.sColor = 255; //white
-  this.sizeUnit = 10;
-  this.velocity = 0.4;
-  this.food = 1;
-  this.body = [this.position];
+function pickLocation(){
+  var cols = Math.floor(width/sizeGlobal);
+  var rows = Math.floor(height/sizeGlobal);
+  food = new p5.Vector(Math.floor(random(cols)), Math.floor(random(rows))); //p5.js reference
+  food.mult(sizeGlobal);
+}
 
-  this.show = function(){
-    this.body.forEach((element) => {
-      fill(255);
-      square(element[0],element[1],this.sizeUnit);
-      console.log(element);
-    });
-      
-  }
+function snake() {
+  this.position = [0, 0]; //x,y
+  this.sColor = 255; //white
+  this.xSpeed = 1;
+  this.ySpeed = 0;
+  this.bodySize = 0;
+  this.body = [];
+
   //gets and sets
-  this.getDirection = function () {
-    return this.keyDirection;
-  }
-  this.setDirection = keyDirection => {
-    this.keyDirection = keyDirection;
-  }
   this.getPosition = function () {
     return this.position;
   }
   this.setPosition = vectorPosition => {
     this.position = vectorPosition;
   }
-  this.updatePosition = function () {
-    if (this.getDirection() === 'W') {
-      this.setPosition([this.position[0], this.position[1] - (this.velocity*this.sizeUnit)]);
-    } else if (this.getDirection() === 'S') {
-      this.setPosition([this.position[0], this.position[1] + (this.velocity*this.sizeUnit)]);
-    } else if (this.getDirection() === 'A') {
-      this.setPosition([this.position[0] - (this.velocity*this.sizeUnit), this.position[1]]);
-    } else if (this.getDirection() === 'D') {
-      this.setPosition([this.position[0] + (this.velocity*this.sizeUnit), this.position[1]]);
-    }
-    this.body.unshift(this.position);
-    this.body.pop();
+  this.setSpeed = function(x,y){
+    this.xSpeed =x;
+    this.ySpeed =y;
   }
-}
+
+  this.show = function(){
+    fill(255);
+    for (let i = 0; i < this.body.length; i++) {
+      square(this.body[i].x,this.body[i].y,sizeGlobal);
+    }
+    fill(255);
+    square(this.position[0],this.position[1],sizeGlobal);
+    }
+
+    this.eat = function(pos){
+      let distance = dist(this.position[0], this.position[1], pos.x, pos.y);
+      return (distance < 1) ? true:false;
+    }
+    this.death = function(){
+      for (let i = 0; i < this.body.length; i++) {
+        let pos = this.body[i];
+        let distance = dist(this.position[0], this.position[1], pos.x, pos.y);
+        if(distance < 1){
+          this.bodySize = 0;
+          this.body = [];
+        }
+      }
+    }
+    this.update = function(){
+      if(this.bodySize === this.body.length){
+        for (let i = 0; i < this.body.length-1; i++) {
+          this.body[i] = this.body[i+1];
+        }
+      }
+      this.body[this.bodySize-1] = new p5.Vector(this.position[0], this.position[1]);
+
+      this.position[0] = this.position[0] + this.xSpeed * sizeGlobal;
+      this.position[1] = this.position[1] + this.ySpeed * sizeGlobal;
+
+      this.position[0] = constrain(this.position[0], 0,width - sizeGlobal);
+      this.position[1] = constrain(this.position[1], 0,height - sizeGlobal); 
+
+    }
+  }
+  
+
