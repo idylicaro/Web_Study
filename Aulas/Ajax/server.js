0074ec2c8b4230ad89912a82ceb5e6fdc9644cd7
Idyl Icaro dos Santos <idylicaro.se@hotmaill.com>
2020-05-15 15:49:59 -0300

    [Update] study  axios2

--- Aulas/Ajax/server.js
+++ Aulas/Ajax/server.js
@@ -36,4 +36,11 @@ app.post('/formulario', (req, res) => {
         id:1
     })
 })
+app.get('/parOuImpar', (req,res) =>{
+    const par = parseInt(req.query.numero) % 2 ===0;
+    res.send({
+        resultado:par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('[RUN SERVER] - Executing...'));
