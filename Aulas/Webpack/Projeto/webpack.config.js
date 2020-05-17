542020c33c6b6fb05782598f7c93e08db4416f47
Idyl Icaro dos Santos <idylicaro.se@hotmaill.com>
2020-05-17 02:08:13 -0300

    [ok] project exemple study

--- Aulas/Webpack/Projeto/webpack.config.js
+++ Aulas/Webpack/Projeto/webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path');
+const path = require('path');
 const webpack = require('webpack');
 const modoDev = process.env.NODE_ENV !== 'production';
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
