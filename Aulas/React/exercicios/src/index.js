3ac5afdd6fc868ee3a74ac2f7e4b2815d1c42559
Idyl Icaro dos Santos <idylicaro.se@hotmaill.com>
2020-05-18 15:01:36 -0300

    [update] react study

--- Aulas/React/exercicios/src/index.js
+++ Aulas/React/exercicios/src/index.js
@@ -3,12 +3,17 @@ import ReactDom from 'react-dom';
 
 //import Primeiro from './componentes/Primeiro';
 //import BomDia from './componentes/BomDia';
-
-import Multi from './componentes/Multiplos'
+//import Multi from './componentes/Multiplos'
+//import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai';
+import Filho from './componentes/Filho';
 
 ReactDom.render(
     <div>
-        <Multi.BoaTarde nome="icaro" />
-        <Multi.BoaNoite nome="igor" />
+        <Pai nome="Higino" sobrenome="Santos">
+            {/* Como passo os componentes filhos aqui? */}
+            <Filho nome="Icaro" />
+            <Filho nome="Carla" />    
+        </Pai>
     </div>
  , document.getElementById('root'));
